505e801145d789a3fce45c0a8ba532cff8b3d684
irisus1 <22520210@gm.uit.edu.vn>
2025-03-21 19:39:53 +0700

    demo homepage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/homepage";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: <RootLayout />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+  // {
+  //   path: "*",
+  //   element: <ErrorPage />,
+  // },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <AuthProvider> */}
+    <RouterProvider router={router}>
+      {/* Đặt UpdateDocumentTitle bên trong RouterProvider */}
+      {/* <UpdateDocumentTitle /> */}
+    </RouterProvider>
+    {/* </AuthProvider> */}
+  </React.StrictMode>
+);
