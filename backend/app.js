503530bb05834e87387e0f559a1d897d46963906
irisus1 <146847404+irisus1@users.noreply.github.com>
2025-05-01 19:38:42 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,14 @@
 const cors = require("cors");
 const express = require("express");
 const app = express();
-<<<<<<< Updated upstream
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 const amenityDetailRoutes = require('./routes/amenityDetailRoutes');
 const amenityRoutes = require('./routes/amenityRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
@@ -23,41 +30,11 @@ const roomTypeRoutes = require('./routes/roomTypeRoutes');
 const serviceRoutes = require('./routes/serviceRoutes');
 const serviceTypeRoutes = require('./routes/serviceTypeRoutes');
 const serviceUsageDetailRoutes = require('./routes/serviceUsageDetailRoutes');
-=======
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
-const amenityRoutes = require("./routes/amenityRoutes");
-const bookingRoutes = require("./routes/bookingRoutes");
-const bookingDetailRoutes = require("./routes/bookingDetailRoutes");
-const branchRoutes = require("./routes/branchRoutes");
-const chatHistoryRoutes = require("./routes/chatHistoryRoutes");
-const chatSessionRoutes = require("./routes/chatSessionRoutes");
-const customerAccountRoutes = require("./routes/customerAccountRoutes");
-const customerRoutes = require("./routes/customerRoutes");
-const employeeAccountRoutes = require("./routes/employeeAccountRoutes");
-const employeeActivityRoutes = require("./routes/employeeActivityRoutes");
-const employeeRoutes = require("./routes/employeeRoutes");
-const invoiceRoutes = require("./routes/invoiceRoutes");
-const nearbyLocationRoutes = require("./routes/nearbyLocationRoutes");
-const roleRoutes = require("./routes/roleRoutes");
-const roomAmenityDetailRoutes = require("./routes/roomAmenityDetailRoutes");
-const roomRoutes = require("./routes/roomRoutes");
-const roomTypeRoutes = require("./routes/roomTypeRoutes");
-const serviceRoutes = require("./routes/serviceRoutes");
-const serviceTypeRoutes = require("./routes/serviceTypeRoutes");
-const serviceUsageDetailRoutes = require("./routes/serviceUsageDetailRoutes");
->>>>>>> Stashed changes
 
 // Middleware
 app.use(express.json());
 
 // Routes
-<<<<<<< Updated upstream
 app.use('/api/amenityDetails', amenityDetailRoutes);
 app.use('/api/amenities', amenityRoutes);
 app.use('/api/bookings', bookingRoutes);
@@ -78,28 +55,7 @@ app.use('/api/roomTypes', roomTypeRoutes);
 app.use('/api/services', serviceRoutes);
 app.use('/api/serviceTypes', serviceTypeRoutes);
 app.use('/api/serviceUsageDetails', serviceUsageDetailRoutes);
-=======
-app.use("/api/amenities", amenityRoutes);
-app.use("/api/bookings", bookingRoutes);
-app.use("/api/bookingDetails", bookingDetailRoutes);
-app.use("/api/branches", branchRoutes);
-app.use("/api/chatHistories", chatHistoryRoutes);
-app.use("/api/chatSessions", chatSessionRoutes);
-app.use("/api/customerAccounts", customerAccountRoutes);
-app.use("/api/customers", customerRoutes);
-app.use("/api/employees", employeeRoutes);
-app.use("/api/employeeAccounts", employeeAccountRoutes);
-app.use("/api/employeeActivities", employeeActivityRoutes);
-app.use("/api/invoices", invoiceRoutes);
-app.use("/api/nearbyLocations", nearbyLocationRoutes);
-app.use("/api/roles", roleRoutes);
-app.use("/api/roomAmenityDetails", roomAmenityDetailRoutes);
-app.use("/api/rooms", roomRoutes);
-app.use("/api/roomTypes", roomTypeRoutes);
-app.use("/api/services", serviceRoutes);
-app.use("/api/serviceTypes", serviceTypeRoutes);
-app.use("/api/serviceUsageDetails", serviceUsageDetailRoutes);
->>>>>>> Stashed changes
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
